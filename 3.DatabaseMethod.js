13fbc0cc4aa58e166747b8cb3fd37b85d3aaba6f
alriansyah <alriansyah1218@gmail.com>
2023-10-12 17:53:13 +0700

    Database method

---
+++ 3.DatabaseMethod.js
@@ -0,0 +1,7 @@
+// Database Methods (mongosh: MongoDB Shell)
+// Database Methods:            Keterangan :
+// - db.dropDatabase()          - Menghapus database
+// - db.getName()               - Mengambil nama database
+// - db.hostInfo()              - Mengambil informasi host tempat mongodb
+// - db.version()               - Mengambil versi database
+// - db.stats()                 - Mengambil statistik penggunaan database
